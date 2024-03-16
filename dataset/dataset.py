3a9caa36fbb75b2fa7cd0100710c8746f910bcf6
Giovanni <g.carbone42@studenti.unisa.it>
2024-03-16 11:56:50 +0100

    Aggiunta del codice per i vari istrogrammi e del grafico a torta.

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 #Trasformazione del dataset da dataframe a CSV con aggiunta del nome delle colonne
 column=['id','diagnosis','radius1','texture1','perimeter1','area1','smoothness1','compactness1','concavity1','concave_points1','symmetry1','fractal_dimension1','radius2','texture2','perimeter2','area2','smoothness2','compactness2','concavity2','concave_points2','symmetry2','fractal_dimension2','radius3','texture3','perimeter3','area3','smoothness3','compactness3','concavity3','concave_points3','symmetry3','fractal_dimension3']
@@ -11,8 +12,6 @@ dataframe = pd.read_csv(data_file_path, names=column, delimiter=',')
 # Salva il dataframe in formato CSV
 csv_file_path = 'wdbc.csv'
 dataframe.to_csv(csv_file_path, index=False)
-
-print(f'Trasformazione completata. File CSV salvato in: {csv_file_path}')
 
 dataset=pd.read_csv('wdbc.csv')
 
@@ -29,9 +28,78 @@ print(dataset['diagnosis'].unique())
 print('Valori mancanti:')
 boolean_dataset= dataset.isna()
 missing_values = boolean_dataset.sum()
-print(missing_values)
-#Il dataset non presenta valori mancanti per ogni colonna
+print(missing_values)#Il dataset non presenta valori mancanti per ogni colonna
 
-#Creazione di due dataset uno per i tumori beningni e uno per quelli maligni
-dataset_malignant=dataset[dataset['diagnosis']=='M']
-dataset_benign=dataset[dataset['diagnosis']=='B']
+# Creazione di due dataset, uno per i tumori benigni e uno per quelli maligni
+dataset_malignant = dataset[dataset['diagnosis'] == 'M']
+dataset_benign = dataset[dataset['diagnosis'] == 'B']
+
+column_to_plot=['diagnosis']
+
+for column in column_to_plot:
+    plt.close("all")
+    unique_values=dataset[column].unique()
+    bins=np.arange(len(unique_values)+1)-0.5
+
+    plt.hist([dataset_benign[column], dataset_malignant[column]], color=["skyblue", "blue"], edgecolor='black', alpha=0.8, stacked=True, rwidth=0.5, bins=bins)
+
+    plt.xlabel(column)
+    plt.ylabel("Frequency")
+
+    plt.legend(["Benign","Malignant"])
+
+    # Salva l'istogramma come immagine
+    plt.savefig(f"{column}_histogram.png")
+
+    # Chiudi la figura per liberare la memoria
+    plt.close()
+
+
+column_to_interest = ['radius1', 'texture1', 'perimeter1', 'area1', 'radius2', 'texture2', 'perimeter2', 'area2', 'radius3', 'texture3', 'perimeter3', 'area3']
+
+for column in column_to_interest:
+    # Chiusura di tutte le figure "aperte" per evitare sovrapposizioni e visualizzazione distorta degli istogrammi
+    plt.close("all")
+
+    # Inizializzazione di un array contenente tutti i valori che la colonna può assumere
+    unique_values = dataset[column].unique()
+    # Inizializzazione di un array contenente valori da 0 al numero di valori della colonna, che corrisponderà al numero
+    # dei bins (barre verticali) contenuti nell'istogramma. Si sottrae 0.5 per centrare il bin rispetto all'etichetta
+    bins = np.arange(len(unique_values) + 1) - 0.5
+
+    # Creazione dell'istogramma
+    plt.hist([dataset_benign[column], dataset_malignant[column]], color=["skyblue", "blue"], edgecolor='black', alpha=0.8, stacked=True, rwidth=0.5, bins=30)
+
+    # Definizione delle etichette presenti sull'asse x e sull'asse y dell'istogramma
+    plt.xlabel(column.replace('-', ' ').capitalize())
+    plt.ylabel("Frequenza")
+    # Definizione della legenda dell'istogramma
+    plt.legend(["Benign","Malignant"])
+
+    # Salva l'istogramma come immagine
+    plt.savefig(f"{column}_histogram.png")
+
+    # Chiudi la figura per liberare la memoria
+    plt.close()
+
+#Calcolo delle correlazione del dataset
+# Trasforma la colonna 'diagnosis' in valori numerici (0 per 'B' e 1 per 'M')
+dataset['diagnosis'] = dataset['diagnosis'].map({'B': 0, 'M': 1})
+
+# Calcola la matrice di correlazione
+correlation_matrix = dataset.corr()
+
+# Crea la heatmap
+plt.figure(figsize=(20,20))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+plt.title('Heatmap della correlazione tra le variabili')
+plt.savefig(f"heatmap.png")
+plt.close()
+
+fig, ax = plt.subplots(figsize=(7, 6))
+# Posizionamento del pie plot all'interno dell'immagine
+fig.subplots_adjust(left=0, right=1, top=1, bottom=0.1)
+# Creazione del pie plot per controllare il bilanciamento della classe da predire
+plt.pie(dataset['diagnosis'].value_counts(), labels=['Benign', 'Malignant'], colors=["skyblue", "blue"],explode=(0, 0.015), autopct="%0.2f", startangle=90, textprops={'fontsize': 11})
+plt.savefig(f"pieplot.png")
+plt.close()
